76a8a42ad5c907d923ac026344cdf4e5c1f80b8e
WonukJeong <ku4295@kakao.com>
2022-01-04 16:41:57 +0900

    초기 커밋.

---
+++ SmartCaraTest/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls.DataVisualization.Charting;
+
+namespace SmartCaraTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Style getLineStyle(SolidColorBrush color)
+        {
+            Style style = new Style(typeof(LineDataPoint));
+            style.Setters.Add(new Setter(LineDataPoint.TemplateProperty, null));
+            style.Setters.Add(new Setter(LineDataPoint.BackgroundProperty, color));
+            return style;
+        }      
+    }
+}
